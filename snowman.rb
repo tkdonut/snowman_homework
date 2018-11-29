f8bd59e7aaffc178e06cfbb9cc0e189ccd027044
tkdonut <tommkendrick@gmail.com>
2018-11-29 01:34:06 +0000

    Added splash to runner

---
+++ snowman.rb
@@ -0,0 +1,26 @@
+require_relative('./game')
+require_relative('./hiddenword')
+require_relative('./player')
+system('clear')
+puts ''
+puts ''
+puts ' .d8888b.                                                                  '
+puts 'd88P  Y88b                                                                 '
+puts 'Y88b.                                                                      '
+puts ' "Y888b.   88888b.   .d88b.  888  888  888 88888b.d88b.   8888b.  88888b.  '
+puts '    "Y88b. 888 "88b d88""88b 888  888  888 888 "888 "88b     "88b 888 "88b '
+puts '      "888 888  888 888  888 888  888  888 888  888  888 .d888888 888  888 '
+puts 'Y88b  d88P 888  888 Y88..88P Y88b 888 d88P 888  888  888 888  888 888  888 '
+puts ' "Y8888P"  888  888  "Y88P"   "Y8888888P"  888  888  888 "Y888888 888  888 '
+
+puts ''
+puts ''
+p "It's definately not hangman"+" - anon"
+puts ''
+puts ''
+puts ''
+puts ''
+puts ''
+print 'What would you like to be called? '
+
+name = gets.chomp
